7848ac93f6e3c48ac3d3ac45dba2b30702fd37e4
AMINE <amineelkahza@gmail.com>
2022-03-28 19:40:18 +0200

    add input texxt

---
+++ src/utilities/seeds.js
@@ -0,0 +1,89 @@
+// Sample messages for testing purpose
+export const messages = [
+  {
+    id: 1,
+    author: 'Thomas',
+    content: 'Lorem ipsum dolor sit amet.'
+  },
+  {
+    id: 2,
+    author: 'Thomas',
+    content: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.'
+  },
+  {
+    id: 3,
+    author: 'Nicolas',
+    content: 'Lorem ipsum dolor sit amet.'
+  },
+  {
+    id: 4,
+    author: 'Thomas',
+    content: 'Lorem ipsum dolor sit amet.'
+  },
+  {
+    id: 5,
+    author: 'Nicolas',
+    content: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.'
+  },
+  {
+    id: 6,
+    author: 'Nicolas',
+    content: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.'
+  }
+]
+
+// Sample rooms and messages for testing purpose
+export const rooms = [
+  {
+    id: 1,
+    title: 'Général',
+    messages: [
+      {
+        id: 1,
+        author: 'Thomas',
+        content: 'Lorem ipsum dolor sit amet.'
+      },
+      {
+        id: 2,
+        author: 'Thomas',
+        content: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.'
+      },
+      {
+        id: 3,
+        author: 'Nicolas',
+        content: 'Lorem ipsum dolor sit amet.'
+      },
+      {
+        id: 4,
+        author: 'Thomas',
+        content: 'Lorem ipsum dolor sit amet.'
+      },
+      {
+        id: 5,
+        author: 'Nicolas',
+        content: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.'
+      },
+      {
+        id: 6,
+        author: 'Nicolas',
+        content: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.'
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: 'Bugs',
+    messages: [
+      {
+        id: 1,
+        author: 'Laurent',
+        content: 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.'
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: 'Releases',
+    messages: []
+  }
+]
